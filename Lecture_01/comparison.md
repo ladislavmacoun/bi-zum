7daae1082bfc8571fb1891ee426feb68682b7d2b
Ladislav Macoun <ladislavmacoun@gmail.com>
2018-03-08 18:22:38 +0100

    Lecture 1

---
+++ Lecture_01/comparison.md
@@ -0,0 +1,7 @@
+# Porovnání DFS a BFS
+## BFS
+Slozitost 
+
+## DFS
+Casova slozitost : O(|V| + |E|)
+Pamětová složitost : O(|V| + |E|)
---
+++ Lecture_01/graphs_generator.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pdread_csv('graph_table.csv', sep=',', header=1)
+df.values
